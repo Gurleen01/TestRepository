5886cdc95ef6cd3a651fb4cb65b2596169ca20f5
Gurleen <gurleen@monamitech.com>
2018-09-25 15:04:42 +0530

    [Rel] Defect id/ Dev - Feature developed.

---
+++ SampleGitProject/SampleGitProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SampleGitProject
+//
+//  Created by vinay on 25/09/18.
+//  Copyright © 2018 com.monamitech.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("GIT Test")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
