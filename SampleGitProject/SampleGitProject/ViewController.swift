8d621bbaae11e062b9a32e40ccd9679ee8c7641f
Gurleen <gurleen@monamitech.com>
2018-09-26 14:49:34 +0530

    [Rel] Defect id/ Dev - Feature developed.

--- SampleGitProject/SampleGitProject/ViewController.swift
+++ SampleGitProject/SampleGitProject/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("GIT Test ")
+        print("GIT Test >>>>>>>>")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
